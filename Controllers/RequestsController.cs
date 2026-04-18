7d1f4245c00c5cc0f45834e52722c4a30b756160
Lidya Abdi <lidyaabdi.dev@gmail.com>
2026-04-18 18:47:53 +0300

    Add Create request form and POST action

---
+++ Controllers/RequestsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InternalRequestSystem.Models;
+
+namespace InternalRequestSystem.Controllers
+{
+    public class RequestsController : Controller
+    {
+        /*
+         Index  	رح تعرض كل الطلبات
+        Create  	رح تعرض صفحة إنشاء طلب  
+        */
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        /*
+         ***صفحة Create***
+         فورم
+         Controller يستقبل البيانات
+        */
+        [HttpPost]
+        public IActionResult Create(Request request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(request);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}

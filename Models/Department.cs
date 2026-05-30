ffdbda96337244afcbb2b4bbf20bbb27ac0c8b25
Lidya Abdi <lidyaabdi.dev@gmail.com>
2026-05-30 16:44:06 +0300

    Add validation attributes to department and role models

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,11 @@
-﻿namespace InternalRequestSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternalRequestSystem.Models
 {
     public class Department
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Department name is required.")]
         public string DepartmentName { get; set; } = string.Empty;
 
         public List<AppUser> Users { get; set; } = new List<AppUser>();
--- Models/Role.cs
+++ Models/Role.cs
@@ -1,8 +1,12 @@
-﻿namespace InternalRequestSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternalRequestSystem.Models
 {
     public class Role
     {
+
         public int Id { get; set; }
+        [Required(ErrorMessage = "Role name is required.")]
         public string RoleName { get; set; } = string.Empty;
         public List<AppUser> Users { get; set; } = new List<AppUser>();
     }

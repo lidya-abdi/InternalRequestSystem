0fb3227cfafb200e4094b34b5d94ed9fc88b7727
Lidya Abdi <lidyaabdi.dev@gmail.com>
2026-05-31 11:57:10 +0300

    Add EF Core migration updates

---
+++ Migrations/20260530175154_ImproveValidationAndLogRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternalRequestSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImproveValidationAndLogRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
